775a12fdf17b97529fed6e682b7a52622efe3159
jsteward <jeremiahsteward@yahoo.com>
2023-01-05 23:41:32 -0600

    Multiple new maps created for the Frost Caverns.
    Some modification to Pallet Town made, as well as the start of the US Gym.

--- data/maps/objects/FrostCavern.asm
+++ data/maps/objects/FrostCavern.asm
@@ -11,7 +11,7 @@ FrostCavern_Object:
 	bg_event 17, 13, 2
 
 	def_object_events
-	object_event 31, 14, SPRITE_POKE_BALL, STAY, NONE, 1, NUGGET
-	object_event 25, 14, SPRITE_POKE_BALL, STAY, NONE, 2, NUGGET
+	object_event 23, 14, SPRITE_POKE_BALL, STAY, NONE, 1, NUGGET
+	object_event 17, 14, SPRITE_POKE_BALL, STAY, NONE, 2, NUGGET
 
 	def_warps_to FROST_CAVERN
--- data/maps/objects/PalletTown.asm
+++ data/maps/objects/PalletTown.asm
@@ -2,20 +2,20 @@ PalletTown_Object:
 	db $b ; border block
 
 	def_warp_events
-	warp_event  5,  5, REDS_HOUSE_1F, 1
-	warp_event 13,  5, BLUES_HOUSE, 1
-	warp_event 12, 11, OAKS_LAB, 2
-	warp_event  5, 11, PALLET_DOCK, 1
+	warp_event 25,  5, REDS_HOUSE_1F, 1
+	warp_event 33,  5, BLUES_HOUSE, 1
+	warp_event 32, 11, OAKS_LAB, 2
+	;warp_event  5, 11, PALLET_DOCK, 1
 
 	def_bg_events
-	bg_event 13, 13, 4 ; PalletTownText4
-	bg_event  7,  9, 5 ; PalletTownText5
-	bg_event  3,  5, 6 ; PalletTownText6
-	bg_event 11,  5, 7 ; PalletTownText7
+	bg_event 33, 13, 4 ; PalletTownText4
+	bg_event 27,  9, 5 ; PalletTownText5
+	bg_event 23,  5, 6 ; PalletTownText6
+	bg_event 31,  5, 7 ; PalletTownText7
 
 	def_object_events
-	object_event  8,  5, SPRITE_OAK, STAY, NONE, 1 ; person
-	object_event  3,  8, SPRITE_GIRL, WALK, ANY_DIR, 2 ; person
-	object_event 11, 14, SPRITE_FISHER, WALK, ANY_DIR, 3 ; person
+	object_event 28,  5, SPRITE_OAK, STAY, NONE, 1 ; person
+	object_event 23,  8, SPRITE_GIRL, WALK, ANY_DIR, 2 ; person
+	object_event 31, 14, SPRITE_FISHER, WALK, ANY_DIR, 3 ; person
 
 	def_warps_to PALLET_TOWN
